21077d30171ebda16ec03a97140344f402661e94
Mohamed Nour Abdo <abdonoor684@gmail.com>
2025-04-23 15:03:31 +0300

    Upload 001_Print_Statement

---
+++ 001_Print_Statement/001_Print_Statement.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // Print Statement
+    // Syntax1: cout<<" What Do You Need To Print It? ";
+    // Syntax2: printf(" What Do You Need To Print It? ", Variables Is Found);
+    // Examples:
+    cout<<"Hello Mohamed, How Are You Today?"<<endl;
+    printf("Hi Abdo, I am fine, Thanks About That Quastion");
+    return 0;
+}
