3916897a925de2a59c508e7878fed2bdc786f554
Mohamed Nour Abdo <abdonoor684@gmail.com>
2025-04-23 18:59:31 +0300

    Upload 003_DataTypes&Variables

---
+++ 003_DataTypes&Variables/003_DataTypes&Variables.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    // DataTypes:
+    /*
+    1 - int x = 10; ===> 4 Or 2 Bytes
+    2 - short x = 20; ===> 2 Byte
+    3 - float x = 3.1; ===> 4 Bytes
+    4 - double x = 13.54; ===> 8 Bytes
+    5 - char x = '\0'; ===> 1 byte
+    6 - string x = "Mohamed"; ===> 1 byte for every letter
+    7 - bool x = false; ===> 1 byte
+    */
+    // Variables: type name = value;
+    // Rule For Named Variables:
+    /*
+    1 - áÇ íÌÈ Ãä íÈÏÃ ÈÑÞã
+    2 - áÇ íÌÈ Çä íÍæí ÃÍÏ ÇáÑãæÒ ÇáÎÇÕÉ
+    3 - íãßä Ãä íãáß ÃÍ ÇáÑãÒíä ÝÞØ $ _
+    4 - Initialization ===> int x = 10;
+    5 - Declaration ===> int x;
+    */
+    // Examples:
+    int x1 = 10;
+    short x2 = 20;
+    float x3 = 3.14;
+    double x4 = 10.2;
+    bool x5 = true;
+    char x6 = 'A';
+    string x7 = "Mohamed";
+    cout<<x1<<endl;
+    cout<<x2<<endl;
+    cout<<x3<<endl;
+    cout<<x4<<endl;
+    cout<<x5<<endl;
+    cout<<x6<<endl;
+    cout<<x7<<endl;
+    return 0;
+}
