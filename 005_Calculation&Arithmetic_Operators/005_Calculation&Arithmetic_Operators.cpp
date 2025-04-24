019e07cce6567832c9ac9d42f256668bc2d5363b
Mohamed Nour Abdo <abdonoor684@gmail.com>
2025-04-24 15:49:58 +0300

    Upload 005_Calculation&Arithmetic_Operators

---
+++ 005_Calculation&Arithmetic_Operators/005_Calculation&Arithmetic_Operators.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // Calculation&Arithmetic_Operators
+    // Arithmetic Operators:
+    // + Addition
+    // - Subtraction
+    // * Multiplication
+    // / Division
+    // ** Exponentiation
+    // % Modular Arithmetic
+
+    //Examples About Calculation:
+    // 1 - Sum Two Numbers
+    int x=0,y=0,z=0;
+    cin>>x>>y;
+    z = x + y;
+    cout<<"The Sum z = "<<z<<endl;
+    cout<<"The Sum x + y = "<<x + y<<endl;
+    // Note:
+    // Always The Sum Return For Strong DataType
+    // Example: int + float + double = double(Answer)
+    // int strong than float
+    // double strong than int, float
+    return 0;
+    system("PAUSE");
+}
