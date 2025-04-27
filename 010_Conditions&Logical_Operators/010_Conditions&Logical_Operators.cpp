bc8803e909537f32f89bda33abba7446adfd0b59
Mohamed Nour Abdo <abdonoor684@gmail.com>
2025-04-27 23:54:59 +0300

    Upload 010_Conditions&Logical_Operators

---
+++ 010_Conditions&Logical_Operators/010_Conditions&Logical_Operators.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // 1 - Conditions
+    // >= <= < > == === != <>
+    // 2 - Logical Operators
+    // And (&) - Or (|) - Not (!)
+
+    return 0;
+    system("PAUSE");
+}
