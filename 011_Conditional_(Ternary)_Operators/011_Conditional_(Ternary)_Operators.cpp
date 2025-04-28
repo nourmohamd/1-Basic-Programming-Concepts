9c1e20e88eab2dfec00d5f3b2653e816de5ee5b9
Mohamed Nour Abdo <abdonoor684@gmail.com>
2025-04-28 20:32:28 +0300

    Upload 011_Conditional_(Ternary)_Operators

---
+++ 011_Conditional_(Ternary)_Operators/011_Conditional_(Ternary)_Operators.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // Conditional (Ternary) Operators
+    // Syntax: (Condition)?True:False;
+    // Examples:
+    // Ex1:
+    int x = (3 > 0)?3:0;
+    cout<<x<<endl;
+    // Ex2:
+    cout<<(5<10)?"Yes":"No";
+
+    return 0;
+    system("PAUSE");
+}
