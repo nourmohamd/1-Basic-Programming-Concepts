fcbbdebf4a7b3ea997d12e36f8d10d58ef86df4d
Mohamed Nour Abdo <abdonoor684@gmail.com>
2025-05-03 23:04:42 +0300

    Upload 017_Comments

---
+++ 017_Comments/017_Comments.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // Comments
+    // 1 -
+    // Single Comment
+    // 2 -
+    /*
+        Multi Comments
+    */
+    return 0;
+    system("PAUSE");
+}
