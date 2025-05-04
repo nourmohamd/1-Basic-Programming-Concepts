e9f0aa43f08d2384555c609fd38968d422c5e476
Mohamed Nour Abdo <abdonoor684@gmail.com>
2025-05-04 22:49:24 +0300

    Upload 019_Function(Built_In_Function)

---
+++ 019_Function(Built_In_Function)/019_Function(Built_In_Function).cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // Function Built_In
+    // It is Functions Built In C++ Language By Default
+    // Example:
+    /*
+        1 - Mathmateics: Needed #include <cmath>
+        ====================================
+        sqrt(x) ===> For Sqrt
+        pow(x,y) ===> For Power
+        sin(x) ===> For Sin
+        cos(x) ===> For Cos
+        tan(x) ===> For Tan
+        ceil(x) ===> For Ceilling Number
+        floor(x) ===> For Floor Number
+        abs(x) ===> For Absolute Number
+        fabs(x) ===> For Absolute Number
+        log(x) ===> For Logarithm
+        exp(x) ===> For Exp
+        toupper(x) ===> For UpperCase
+        tolower(x) ===> For LowerCase
+
+        2 - Algorithm: Needed #include <algorithm>
+        ==========================================
+        max(1,max(3,5)) ===> For Get Max Value
+        min(1,min(3,5)) ===> For Get Min Value
+        swap(x,y) ===> For Swap Between Two Numbers
+
+        3 - rand: Needed #include <cstdlib>
+        ===================================
+        for get random numbers
+        // Example:
+        // 1
+        cout<<rand()<<endl;
+        // 2
+        for(int i=0;i<10;i++)
+            cout<<rand()<<endl;
+        // 3
+        for(int i=0;i<10;i++)
+            cout<<rand()%10+0<<endl; // 0 ---> 10
+        // 4
+        for(int i=0;i<10;i++)
+            cout<<rand()%10+1<<endl; // 1 ---> 10
+        // (big - small + 1) + small
+        // 5
+        for(int i=0;i<10;i++)
+            cout<<rand()%(80 - 40 + 1) + 40<<endl;
+
+        4 - time: ===> Needed #include <ctime>
+        For Change Time In Your WorkFlow
+        ================================================
+        srand(time(NULL|0));
+        for(int i=0;i<10;i++)
+            cout<<rand()%(80 - 40 + 1) + 40<<endl;
+        // Examles:
+        // 1
+        int x = 0, y = 0;
+        x = time(0);
+        for(int i = 1;i<=10000;i++)
+        {
+            cout<<time(0)<<endl;
+        }
+        y = time(0);
+        cout<<y - x<<endl;
+    */
+
+    return 0;
+    system("PAUSE");
+}
